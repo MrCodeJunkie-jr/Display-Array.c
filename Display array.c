3475b019f1d77485c7d6ea606210dbf310df71b6
Pratik Raut <106788839+MrCodingJunky-jr@users.noreply.github.com>
2022-06-16 10:57:03 +0545

    Add files via upload

---
+++ Display array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int a[8],i;
+	for (i=0;i<8;i++){
+		printf("enter element ");
+		scanf("%d",&a[i]);
+	}
+	for (i=0;i<8;i++){
+		printf("%d\t",a[i]);
+		
+	}
+	return 0;
+	
+}
